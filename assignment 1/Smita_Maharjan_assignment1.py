3fdb3891386f27725524f75befc860f628ea9302
smita <smitamhz13@gmail.com>
2025-12-16 16:07:24 +0545

    Uploaded Assignment_1 Part_1

---
+++ assignment 1/Smita_Maharjan_assignment1.py
@@ -0,0 +1,44 @@
+# Variables and Input:
+# 1. Create a variable my_age and assign your age to it. Print a message using this variable.
+
+my_age = 20
+print(my_age)
+
+# 2. Ask the user to enter their favorite food and print a message incorporating this input.
+
+fav_food = input("enter your favourite food: ")
+print("favourite food: ",fav_food)
+
+# Type Conversion:
+# 1. Convert the string "42" to an integer and print the result.
+
+st = "42"
+print("the integer is",int(st))
+
+# 2. Convert the floating-point number 3.14159 to a string and print the result.
+
+fl = 3.14159
+print("the string is",str(fl))
+
+# Strings:
+# 1. Concatenate the strings "Hello" and "World!" and print the result.
+
+s1 = 'Hello'
+s2 = ' World'
+print("the concatenated string is", s1+s2)
+
+# 2. Use string indexing to extract the third character from the string "Python".
+
+str = "Python"
+print(str[2])
+
+# 3. Take a sentence as input and print only the first five words.
+
+strs = "Life is a winking light in the darkness."
+words = strs.split()
+first_five_words = words[:5]
+result = " ".join(first_five_words)
+print(result)
+
+first_five = ' '.join(strs.split()[:5])
+print(first_five)
