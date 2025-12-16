bf718f07f7b211c1f0ed2015d13b38cb4a035f76
smita <smitamhz13@gmail.com>
2025-12-16 16:19:05 +0545

    Included outputs in Part 1

--- assignment 1/Smita_Maharjan_assignment1.py
+++ assignment 1/Smita_Maharjan_assignment1.py
@@ -4,10 +4,20 @@
 my_age = 20
 print(my_age)
 
+""" 
+output: 
+20 
+"""
+
 # 2. Ask the user to enter their favorite food and print a message incorporating this input.
 
 fav_food = input("enter your favourite food: ")
 print("favourite food: ",fav_food)
+
+""" 
+output:enter your favourite food: apple
+favourite food:  apple 
+"""
 
 # Type Conversion:
 # 1. Convert the string "42" to an integer and print the result.
@@ -15,10 +25,20 @@ print("favourite food: ",fav_food)
 st = "42"
 print("the integer is",int(st))
 
+"""
+output:
+the integer is 42
+"""
+
 # 2. Convert the floating-point number 3.14159 to a string and print the result.
 
 fl = 3.14159
 print("the string is",str(fl))
+
+"""
+output:
+the string is 3.14159
+"""
 
 # Strings:
 # 1. Concatenate the strings "Hello" and "World!" and print the result.
@@ -27,10 +47,20 @@ s1 = 'Hello'
 s2 = ' World'
 print("the concatenated string is", s1+s2)
 
+"""
+output:
+the concatenated string is Hello World
+"""
+
 # 2. Use string indexing to extract the third character from the string "Python".
 
 str = "Python"
 print(str[2])
+
+"""
+output:
+t
+"""
 
 # 3. Take a sentence as input and print only the first five words.
 
@@ -42,3 +72,9 @@ print(result)
 
 first_five = ' '.join(strs.split()[:5])
 print(first_five)
+
+"""
+output:
+Life is a winking light
+Life is a winking light
+"""
